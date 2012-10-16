805a6c9c0f1dce9dac07a4b5843fdc15fe83d49b
Stas SUȘCOV <stas@net.utcluj.ro>
2012-10-17 00:19:02 +0300

    Cleanup app loader, add more comments.

--- code/js/app.js
+++ code/js/app.js
@@ -1,17 +1,22 @@
 /**
- * Application Loader
+ * Application Loader/Namespace
  * @return Class
  */
 ( function( w ) {
   var App = Ember.Application.create({
-    VERSION: '0.1',
+    VERSION: '0.1.0',
     rootElement: '#content',
     ApplicationController: Ember.Controller.extend(),
     ArticleController: Ember.Controller.extend(),
     ApplicationView: Ember.View.extend({
       template: Ember.Handlebars.compile( '{{outlet}}' )
     })
+    // The rest of the Models/Controllers/Views
+    // will self append to this namespace
   });
 
-  return w.EmBlog = App;
+  // expose application globally
+  w.EmBlog = App;
+
+  return App;
 } )( window );
