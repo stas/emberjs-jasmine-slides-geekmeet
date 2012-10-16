a219245c7b813dd1da0d3c15561f6125016ae9a0
Stas SUȘCOV <stas@net.utcluj.ro>
2012-10-17 00:18:05 +0300

    Add a `beforeEach` for article controller test.

--- code/js/app/test/controller/article.js
+++ code/js/app/test/controller/article.js
@@ -1,24 +1,27 @@
 // Tests for article screen controller class
 ( function( App ){
   describe( 'ArticleController', function() {
+    var controller, view;
+
+    beforeEach( function(){
+      controller = App.ArticleController.create();
+      view = App.ArticleView.create();
+
+      view.set( 'controller', controller );
+    });
 
     it( 'handles article screen', function() {
       var article = App.Article.create({
         title: 'Testing title', content: 'Some content...'
       });
-      var controller = App.ArticleController.create();
-      var view = App.ArticleView.create();
 
       controller.set( 'content', article );
-      view.set( 'controller', controller );
-
       view.createElement();
 
       expect( view.$().find( 'h3' ).length ).toEqual( 1 );
       expect( view.$().find( 'h3' ).text() ).toEqual( 'Testing title' );
       expect( view.$().find( 'p' ).length ).toEqual( 1 );
       expect( view.$().find( 'p' ).text() ).toEqual( 'Some content...' );
-
     });
 
   });
