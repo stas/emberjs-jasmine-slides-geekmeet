ce96d114130e3977f79a1024343ff0bf5e0785db
Stas SUȘCOV <stas@net.utcluj.ro>
2012-10-16 13:58:16 +0300

    Merge application view into app loader.

--- code/js/app.js
+++ code/js/app.js
@@ -7,7 +7,10 @@
     VERSION: '0.1',
     rootElement: '#content',
     ApplicationController: Ember.Controller.extend(),
-    ArticleController: Ember.Controller.extend()
+    ArticleController: Ember.Controller.extend(),
+    ApplicationView: Ember.View.extend({
+      template: Ember.Handlebars.compile( '{{outlet}}' )
+    })
   });
 
   return w.EmBlog = App;
